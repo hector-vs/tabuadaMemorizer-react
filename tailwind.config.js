fe05b8948b992b22798ada98055314caae3d39ad
furPedroRibeiro <pedrocodejob@gmail.com>
2024-03-01 19:55:46 -0300

    adicionando front-end até a parte de gerar a tabuada

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,tsx,css}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [
+    
+  ],
+}
+
